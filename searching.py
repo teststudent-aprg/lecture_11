d0836666b2f1abadccf9c135849d7f5ac50484a0
Jakub Hejc <hejc@vutbr.cz>
2021-04-19 09:58:08 +0200

    created data reader

--- searching.py
+++ searching.py
@@ -1,20 +1,30 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
 
 
-def read_data(file_name, field):
+def read_data(file_name, key):
     """
     Reads json file and returns sequential data.
     :param file_name: (str), name of json file
-    :param field: (str), field of a dict to return
+    :param key: (str), field of a dict to return
     :return: (list, string),
     """
+    if key not in {"unordered_numbers", "ordered_numbers", "dna_sequence"}:
+        return None
+
     file_path = os.path.join(cwd_path, file_name)
+
+    with open(file_path, 'r') as json_file:
+        seqs = json.load(json_file)
+
+    return seqs[key]
 
 
 def main():
+    read_data(file_name='sequential.json', key='unordered_numbers')
     pass
 
 
